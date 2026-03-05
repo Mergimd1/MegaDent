488402d97d78d1ec1d8ee62986564b00649696d0
Mergimd1 <104101790+Mergimd1@users.noreply.github.com>
2026-03-05 22:33:03 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,14 @@
 
+import './globals.css'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="sq">
+      <body>{children}</body>
+    </html>
+  )
+}
